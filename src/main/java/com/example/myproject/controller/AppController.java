a7dbdf00b7d48e430750b7cf144f1a174e90f9f6
Jonnadula Venkata Sai Tanish <venkatasaitanish@gmail.com>
2023-10-16 12:52:53 +0530

    initial commit

---
+++ src/main/java/com/example/myproject/controller/AppController.java
@@ -0,0 +1,12 @@
+package com.example.myproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController{
+    @GetMapping
+    public String getHello(){
+        return "Hello";
+    }
+}
